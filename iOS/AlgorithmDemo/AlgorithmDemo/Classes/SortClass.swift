b91a09c0262f7faf1b5990335965bed83c205b9b
shenyiSS <shenyi1101@gmail.com>
2018-09-05 22:09:06 +0800

    add bubbleSort

--- iOS/AlgorithmDemo/AlgorithmDemo/Classes/SortClass.swift
+++ iOS/AlgorithmDemo/AlgorithmDemo/Classes/SortClass.swift
@@ -13,10 +13,10 @@ class SortClass: NSObject {
 
 }
 
-//MARK:-快速排序
+//MARK:-排序
 extension SortClass {
-
-    static func quickSort(array: inout[Int], low: Int, high: Int) -> [Int] {
+    //快速排序
+    static func quickSort(_ array: inout[Int], _ low: Int, _ high: Int) -> [Int] {
         var left = low;
         var right = high;
         if left >= right {
@@ -40,9 +40,29 @@ extension SortClass {
         }
         
         array[left] = key;
-        quickSort(array: &array, low: low, high: left-1);
-        quickSort(array: &array, low: right+1, high: high);
+        quickSort(&array, low, left-1);
+        quickSort(&array, right+1, high);
         
         return array;
     }
+    
+    //冒泡排序
+    static func bubbleSort(_ array: inout[Int]) -> [Int] {
+        times = 0;
+        
+        let n = array.count;
+        for i in 0..<n {
+            for j in 0..<(n - 1 - i) {
+                if array[j] > array[j+1] {
+                    array.swapAt(j, j+1);
+                    
+                    times += 1;
+                    print("第\(times)次----：\(array)");
+                }
+            }
+        }
+        return array;
+    }
+    
+    
 }
--- iOS/AlgorithmDemo/AlgorithmDemo/ViewController.swift
+++ iOS/AlgorithmDemo/AlgorithmDemo/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var array = [1,3,4,6,8,2,34,5,7,23,5,6,23,55,5,1,1,2,8,0];
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,12 +17,16 @@ class ViewController: UIViewController {
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        testQuickSort();
+//        testQuickSort();
+        testBubbleSort();
     }
     
     func testQuickSort() {
-        var array = [1,3,4,6,8,2,34,5,7,23,5,6,23,55,5,1,1,2,8,0];
-        SortClass.quickSort(array: &array, low: 0, high: array.count-1);
+        SortClass.quickSort(&array, 0, array.count-1);
+    }
+    
+    func testBubbleSort() {
+        SortClass.bubbleSort(&array);
     }
 
 }
